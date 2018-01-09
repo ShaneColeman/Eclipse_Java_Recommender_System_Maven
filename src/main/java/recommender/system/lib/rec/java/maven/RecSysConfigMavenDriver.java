b2a17e85e257ef26b8e35010b6f88af66b3b4835
ShaneColeman <shane.p.coleman@students.ittralee.ie>
2018-01-09 16:27:37 +0000

    Minor code adjustment made to RecSysConfigMavenDriver.java

--- src/main/java/recommender/system/lib/rec/java/maven/RecSysConfigMavenDriver.java
+++ src/main/java/recommender/system/lib/rec/java/maven/RecSysConfigMavenDriver.java
@@ -20,7 +20,7 @@ public class RecSysConfigMavenDriver
 	//public static String CONFIGURATION_FILE = "conf/ConstantGuess-Baseline.properties";
 	//public static String CONFIGURATION_FILE = "conf/GlobalAverage-Baseline.properties";
 	//public static String CONFIGURATION_FILE = "conf/UserAverage-Baseline.properties";
-	public static String CONFIGURATION_FILE = "conf/ItemAverage-Baseline.properties";
+	//public static String CONFIGURATION_FILE = "conf/ItemAverage-Baseline.properties";
 	
 	/*
 	 * ProbabilisticGraphicalRecommender
@@ -38,7 +38,7 @@ public class RecSysConfigMavenDriver
 	 * ItemKNNRecommender 
 	 */
 	//public static String CONFIGURATION_FILE = "conf/UserKNN-CF.properties";
-	//public static String CONFIGURATION_FILE = "conf/ItemKNN-CF.properties";
+	public static String CONFIGURATION_FILE = "conf/ItemKNN-CF.properties";
 	
 	/*
 	 * AbstractRecommender
